e16cc0574b93bc6633aaf4e664ff126e214025a2
Bon Nicolai Mercado <bonhokage06@gmail.com>
2021-10-22 09:57:39 +0000

    update log_file in config

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export default {
     COMPANION_PKID: "ela.companion",// current companion package id,
-    LOG_FILE: "elabox.log"     // the log file
+    LOG_FILE: "log/elabox.log"     // the log file
 }
